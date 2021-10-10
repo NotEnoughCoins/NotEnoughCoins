3ae85a417ab0e1ec90602deaacc14c6a16f13c69
mindlesslydev <adamsecunda2005@gmail.com>
2021-10-10 11:40:08 +0100

    Replaced incorrect event

--- src/main/java/me/mindlessly/notenoughcoins/events/OnWorldJoin.java
+++ src/main/java/me/mindlessly/notenoughcoins/events/OnWorldJoin.java
@@ -2,24 +2,19 @@ package me.mindlessly.notenoughcoins.events;
 
 import me.mindlessly.notenoughcoins.commands.Flip;
 import me.mindlessly.notenoughcoins.utils.ConfigHandler;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
 public class OnWorldJoin {
 	
 	
 	@SubscribeEvent
-    public void onEntityJoinWorld(EntityJoinWorldEvent event) {
-		
-		if(event.world.isRemote) {
-			if(!(event.entity instanceof EntityPlayer)) {
-				return;
-			}
+    public void onEntityJoinWorld(PlayerLoggedInEvent event) {
 			
-			EntityPlayer player = (EntityPlayer) event.entity;
+			EntityPlayer player = (EntityPlayer) event.player;
 			if(ConfigHandler.hasKey(Configuration.CATEGORY_GENERAL, "Flip")){
 	    		    Flip.flip(player);
 			}
