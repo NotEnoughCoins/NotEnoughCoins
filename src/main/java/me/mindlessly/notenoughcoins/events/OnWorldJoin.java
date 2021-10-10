328411e61886c809f81d26336c1f5298a0f99611
mindlesslydev <adamsecunda2005@gmail.com>
2021-10-10 18:15:32 +0100

    Added delay on flipper toggling when joining server

--- src/main/java/me/mindlessly/notenoughcoins/events/OnWorldJoin.java
+++ src/main/java/me/mindlessly/notenoughcoins/events/OnWorldJoin.java
@@ -1,5 +1,8 @@
 package me.mindlessly.notenoughcoins.events;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import me.mindlessly.notenoughcoins.commands.Flip;
 import me.mindlessly.notenoughcoins.utils.ConfigHandler;
 import net.minecraft.client.Minecraft;
@@ -12,9 +15,17 @@ public class OnWorldJoin {
 	
 	@SubscribeEvent
     public void onEntityJoinWorld(EntityJoinWorldEvent event) {
-		if (event.world.isRemote && event.entity == Minecraft.getMinecraft().thePlayer) {
+		if (!event.world.isRemote && event.entity == Minecraft.getMinecraft().thePlayer) {
 			if(ConfigHandler.hasKey(Configuration.CATEGORY_GENERAL, "Flip")){
-    		    Flip.flip(Minecraft.getMinecraft().thePlayer);
+				Timer timer = new Timer();
+				  timer.schedule(
+		            new TimerTask() {
+		              @Override
+		              public void run() {
+		      		    Flip.flip(Minecraft.getMinecraft().thePlayer);
+		              }
+		            },
+		            2000);
 			}
 		}
 	}
