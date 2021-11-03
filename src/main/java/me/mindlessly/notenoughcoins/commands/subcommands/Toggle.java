eddb5a167535b2c1651247bc7386fe984bd8b747
RobotHanzo <nathanlee302@outlook.com>
2021-11-04 00:22:15 +0800

    Fixed the duplication issues (I hope)

--- src/main/java/me/mindlessly/notenoughcoins/commands/subcommands/Toggle.java
+++ src/main/java/me/mindlessly/notenoughcoins/commands/subcommands/Toggle.java
@@ -29,6 +29,7 @@ public class Toggle implements Subcommand {
     public static LinkedHashMap<String, Double> secondDataset = new LinkedHashMap<>();
     public static LinkedHashMap<String, Double> namedDataset = new LinkedHashMap<>();
     public static LinkedHashMap<Integer, Long> updatedDataset = new LinkedHashMap<>();
+    public static ArrayList<String> ignoredUUIDs = new ArrayList<>();
     public static double purse;
     public static ArrayList<String> commands = new ArrayList<>();
     private static int auctionPages = 0;
@@ -90,7 +91,7 @@ public class Toggle implements Subcommand {
         for (int iterate = start; iterate < auctionPages; iterate += increment) {
             final int page = iterate;
             scheduledExecutorService.scheduleAtFixedRate(() -> {
-                boolean print = ApiHandler.getFlips(secondDataset, page, commands);
+                boolean print = ApiHandler.getFlips(secondDataset, page, commands, ignoredUUIDs);
                 if (print) {
                     if (namedDataset.size() > 0) {
                         purse = Math.round(purse);
