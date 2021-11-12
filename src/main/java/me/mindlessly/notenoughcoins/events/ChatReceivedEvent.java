4d1faeeb8f30202bb1e736a4b04ffd0a7fb6ed66
Cephetir <silyichev@gmail.com>
2021-11-12 21:40:55 +0300

    Api key fix

--- src/main/java/me/mindlessly/notenoughcoins/events/ChatReceivedEvent.java
+++ src/main/java/me/mindlessly/notenoughcoins/events/ChatReceivedEvent.java
@@ -6,16 +6,17 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import javax.annotation.Nonnull;
 
 public class ChatReceivedEvent {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void chat(@Nonnull ClientChatReceivedEvent event) {
         if(!event.message.getUnformattedText().startsWith("Your new API key is ")) return;
-        String key = event.message.getChatStyle().getChatClickEvent().getValue();
-        ConfigHandler.writeConfig(Configuration.CATEGORY_GENERAL, "APIKey", key);
-        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + ("NEC ") + "§aAPI Key set to " + key));
+        String[] key = event.message.getUnformattedText().split("key is §2");
+        ConfigHandler.writeConfig(Configuration.CATEGORY_GENERAL, "APIKey", key[1]);
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + ("NEC ") + "§aAPI Key set to " + key[1]));
     }
 }
