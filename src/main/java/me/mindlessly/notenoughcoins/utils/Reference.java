bc4b90635ecffc0510b6b3664d668903b31c3bfd
mindlesslydev <adamsecunda2005@gmail.com>
2021-11-12 18:56:24 +0000

    Updated vers number

--- src/main/java/me/mindlessly/notenoughcoins/utils/Reference.java
+++ src/main/java/me/mindlessly/notenoughcoins/utils/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
     public static final String MOD_ID = "nec";
     public static final String NAME = "NotEnoughCoins";
-    public static final String VERSION = "v0.6.1";
+    public static final String VERSION = "v0.6.2";
 
     public static final Logger logger = LogManager.getLogger("NotEnoughCoins");
 }
