9e84cde26f96ccd32b27bc6ea3a79a235b93882c
mindlesslydev <adamsecunda2005@gmail.com>
2021-11-15 18:46:34 +0000

    Formatting

--- src/main/java/me/mindlessly/notenoughcoins/Reference.java
+++ src/main/java/me/mindlessly/notenoughcoins/Reference.java
@@ -8,6 +8,5 @@ public class Reference {
     public static final String MOD_ID = "nec";
     public static final String NAME = "NotEnoughCoins";
     public static final String VERSION = "v@version@";
-
     public static final Logger logger = LogManager.getLogger("NotEnoughCoins");
 }
